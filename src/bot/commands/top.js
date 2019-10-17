c6c8d584983b07d0ce28fd06f066f950e0cc7100
ironsoul0 <timka2609@gmail.com>
2019-10-17 10:03:17 +0600

    feat: top command is little bit modified

--- src/bot/commands/top.js
+++ src/bot/commands/top.js
@@ -2,25 +2,59 @@ const User = require("../../models/user");
 const students = require("../../data/students.json");
 
 const gameNotStarted = [
-  "We are starting soon..",
+  "This command is not available yet..",
   "Wait while the clock strikes on Halloween 🎃",
 ].join("\n\n");
 
+const convert = value => {
+  return String(value).padStart(2, "0");
+};
+
 module.exports = bot => {
   bot.hears("🔥 TOP Hunters", async ctx => {
-    if (!ctx.gameStarted) {
+    if (ctx.gameStarted) {
       return ctx.reply(gameNotStarted);
     }
-    const users = await User.find({}, ["kills", "pid"], {
-      skip: 0,
-      limit: 10,
-      sort: {
-        kills: -1,
-      },
+    const users = await User.find();
+    let topUsers = users.filter(user => user.kills > 0);
+
+    topUsers.sort((x, y) => {
+      if (
+        x.kills < y.kills ||
+        (x.kills === y.kills && x.lastKill > y.lastKill)
+      ) {
+        return 1;
+      }
+      if (
+        x.kills > y.kills ||
+        (x.kills === y.kills && x.lastKill < y.lastKill)
+      ) {
+        return -1;
+      }
+      return 0;
     });
-    const top = users.map(
-      (el, i) => `${i + 1}. ${students[el.pid]} \t ${el.kills}`
+
+    topUsers = topUsers.slice(0, 9).map((user, index) => {
+      const killDate = user.lastKill;
+      const prefix = `${convert(killDate.getDate())}/${convert(
+        killDate.getMonth()
+      )}`;
+      const suffix = `${convert(killDate.getHours())}:${convert(
+        killDate.getMinutes()
+      )}`;
+
+      const { name } = students[user.pid];
+      const { kills } = user;
+      const lastKill = `${prefix} at ${suffix}`;
+
+      return `${index + 1}. ${name} - <b>${kills}</b> kills - ${lastKill}`;
+    });
+
+    if (topUsers.length === 0) {
+      return ctx.reply("Seems there are no any kills yet.. 🤭");
+    }
+    return ctx.replyWithHTML(
+      `Most dangerous hunters 😈\n\n${topUsers.join("\n")}`
     );
-    return ctx.reply(top.join("\n"));
   });
 };
